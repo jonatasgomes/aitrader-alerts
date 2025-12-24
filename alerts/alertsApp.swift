4271b850c9bcd373b6176adec1ed2b73cddb4e6f
Jonatas Gomes Alves <jonatasgomes@gmail.com>
2025-12-23 20:15:47 -0500

    Initial Commit

---
+++ alerts/alertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  alertsApp.swift
+//  alerts
+//
+//  Created by Jonatas Gomes on 2025-12-23.
+//
+
+import SwiftUI
+
+@main
+struct alertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
